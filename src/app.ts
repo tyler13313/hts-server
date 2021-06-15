f49928b1162b61ff7ad1d0bf18ca0dfd01d21f55
tyler13313 <tyler13313@naver.com>
2021-06-15 21:57:09 +0900

    서버 연습1

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { createServer, Server } from 'http';
 
+import controller from './controller';
+
 const app = express();
+
+app.use(controller);
 
 const server = createServer(app);
 server.listen(process.env.PORT || 5000);
